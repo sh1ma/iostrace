db3dba0f4ad8a7624f91485c32d0048e4c081cd5
4masaka <in9lude@gmail.com>
2020-05-06 21:38:58 +0900

    完成〜

---
+++ README.md
@@ -0,0 +1,9 @@
+# Usage
+
+```
+python3 main.py Twitter
+
+# or
+
+python3 main.py com.atebits.Tweetie2
+```
--- main.py
+++ main.py
@@ -5,18 +5,25 @@ def on_message(meesage, _):
     print(meesage["payload"])
 
 
+def on_detached():
+    sys.exit()
+
+
 def main(target: str) -> None:
     device = frida.get_usb_device()
     apps = device.enumerate_applications()
-    print(apps)
     for app in apps:
         if target == app.identifier or target == app.name:
             app_identifier: str = app.identifier
             break
+
     pid = device.spawn([app_identifier])
     session = device.attach(pid)
+    session.on('detached', on_detached)
+
     with open("tracer.js", "r") as f:
         tracer_source = f.read()
+
     script = session.create_script(tracer_source)
     script.load()
     script.on("message", on_message)
@@ -27,7 +34,7 @@ def main(target: str) -> None:
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="frida-iOS-syscall-tracer")
-    parser.add_argument("target", help="target Process name or Bundle identifier")
+    parser.add_argument("target", help="target process name or bundle identifier")
     args = parser.parse_args()
 
     main(args.target)
